af2b31dafb7e5cc0461b477d3e2144a038d812a8
luvcodechen <chen_ney@qq.com>
2023-08-02 22:44:04 +0800

    java practice day56

--- src/machineLearning/kmeans/KMeans.java
+++ src/machineLearning/kmeans/KMeans.java
@@ -1,5 +1,7 @@
 package machineLearning.kmeans;
 
+import java.util.Random;
+
 /**
  * ClassName: KMeans
  * Package: machineLearning.kmeans
@@ -9,5 +11,20 @@ package machineLearning.kmeans;
  * @Create: 2023/7/31 12:29
  */
 public class KMeans {
+    /**
+     * Manhattan distance.
+     */
+    public static final int MANHATTAN = 0;
+
+    /**
+     * Euclidean distance.
+     */
+    public static final int EUCLIDEAN=1;
+
+    /**
+     * A random instance.
+     */
+    public static final Random RANDOM=new Random();
+
 
 }
