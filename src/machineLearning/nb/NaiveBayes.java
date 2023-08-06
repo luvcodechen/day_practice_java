6750bde19e2f3b444bde110896b396a1b20063d7
luvcodechen <chen_ney@qq.com>
2023-08-06 21:15:57 +0800

    java practice day58

--- src/machineLearning/nb/NaiveBayes.java
+++ src/machineLearning/nb/NaiveBayes.java
@@ -13,6 +13,62 @@ import java.io.FileReader;
  * @Create: 2023/8/4 15:05
  */
 public class NaiveBayes {
+
+    /**
+     * An inner class to store parameters.
+     */
+    private static class GaussianParameters {
+        double mu;
+        double sigma;
+
+        public GaussianParameters(double paraMu, double paraSigma) {
+            mu = paraMu;
+            sigma = paraSigma;
+        }// OF the constructor
+
+        @Override
+        public String toString() {
+            return "GaussianParameters{}";
+        }// Of toString
+    }// OF class GaussianParameters
+
+    /**
+     * The data.
+     */
+    Instances dataset;
+
+    /**
+     * The number of classes. For binary classification it is 2.
+     */
+    int numClasses;
+
+    /**
+     * The number of conditional attributes.
+     */
+    int numConditions;
+
+    /**
+     * The prediction, including queried and predicted labels.
+     */
+    int[]predicts;
+
+    /**
+     * Class distribution.
+     */
+    double[]classDistribution;
+
+    double[]classDistributionLaplacian;
+
+
+    /**
+     * @param tempDataset
+     * @param paraTrainingFraction
+     * @return
+     */
+    private static Instances[] splitTrainingTesting(Instances tempDataset, double paraTrainingFraction) {
+        return new Instances[0];
+    }
+
     /**
      * The entrance of the program.
      *
@@ -29,15 +85,14 @@ public class NaiveBayes {
         Instances tempDataset = null;
         try {
             FileReader fileReader = new FileReader(tempFileName);
-            tempDataset = new Instances(fileReader);    
+            tempDataset = new Instances(fileReader);
             fileReader.close();
         } catch (Exception ee) {
             System.out.println("Cannot read the file :" + tempFileName + "\r\n" + ee);
             System.exit(0);
         }// of try
 
-
-
+        Instances[] tempDatasets = splitTrainingTesting(tempDataset, paraTrainingFraction);
 
 
     }
