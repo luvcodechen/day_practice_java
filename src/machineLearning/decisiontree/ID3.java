1e08b528b0cc2757fd92292881d980231e5fe1c6
luvcodechen <chen_ney@qq.com>
2023-08-08 16:37:22 +0800

    java practice day611

---
+++ src/machineLearning/decisiontree/ID3.java
@@ -0,0 +1,19 @@
+package machineLearning.decisiontree;
+
+import weka.core.Instances;
+
+/**
+ * ClassName: ID3
+ * Package: machineLearning.decisiontree
+ * Description:  The ID3 decision tree inductive algorithm.
+ *
+ * @Author: luv_x_c
+ * @Create: 2023/8/7 14:55
+ */
+public class ID3 {
+    /**
+     * The data.
+     */
+    Instances dataset;
+
+}
