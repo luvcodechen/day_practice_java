86d81ff825b5e2027e45db7c18bfb02fcefa6651
luvcodechen <chen_ney@qq.com>
2023-08-17 20:37:27 +0800

    java practice day64

--- src/machineLearning/adaboosting/SimpleClassifier.java
+++ src/machineLearning/adaboosting/SimpleClassifier.java
@@ -2,7 +2,6 @@ package machineLearning.adaboosting;
 
 import weka.core.Instance;
 
-import java.util.Date;
 import java.util.Random;
 
 /**
@@ -55,8 +54,8 @@ public abstract class SimpleClassifier {
         weightedInstances = paraWeightedInstances;
 
         numConditions = weightedInstances.numAttributes() - 1;
-        numInstances=weightedInstances.numInstances();
-        numClasses=weightedInstances.classAttribute().numValues();
+        numInstances = weightedInstances.numInstances();
+        numClasses = weightedInstances.classAttribute().numValues();
     }// Of the first constructor
 
     /**
@@ -66,6 +65,7 @@ public abstract class SimpleClassifier {
 
     /**
      * Classify an instance.
+     *
      * @param paraInstance The given instance.
      * @return Predicted label.
      */
@@ -73,9 +73,53 @@ public abstract class SimpleClassifier {
 
     /**
      * Which instance in the  training set are correctly classified.
+     *
      * @return The correctness array.
      */
-    public boolean[]computeCorrectnessArray(){
-        boolean[]resultCorrectnessArray=new boolean[]
-    }
-}//Of class
+    public boolean[] computeCorrectnessArray() {
+        boolean[] resultCorrectnessArray = new boolean[weightedInstances.numInstances()];
+        for (int i = 0; i < resultCorrectnessArray.length; i++) {
+            Instance tempInstance = weightedInstances.instance(i);
+            if ((int) (tempInstance.classValue()) == classify(tempInstance)) {
+                resultCorrectnessArray[i] = true;
+            }// OF if
+        }//Of for i
+
+        return resultCorrectnessArray;
+    }//Of computeCorrectnessArray
+
+    /**
+     * Compute the accuracy on the training set.
+     *
+     * @return The training accuracy.
+     */
+    public double computeTrainingAccuracy() {
+        double tempCorrect = 0;
+        boolean[] tempCorrectnessArray = computeCorrectnessArray();
+        for (int i = 0; i < tempCorrectnessArray.length; i++) {
+            if (tempCorrectnessArray[i]) {
+                tempCorrect++;
+            }//Of if
+        }//Of for i
+
+        double resultAccuracy = tempCorrect / tempCorrectnessArray.length;
+
+        return resultAccuracy;
+    }//Of computeTrainingAccuracy
+
+    public double computeWeightedError(){
+        double resultError=0;
+        boolean[]tempCorrectnessArray=computeCorrectnessArray();
+        for (int i = 0; i < tempCorrectnessArray.length; i++) {
+            if(!tempCorrectnessArray[i]){
+                resultError+=weightedInstances.getWeight(i);
+            }//Of if
+        }//Of for i
+
+        if(resultError<1e-6){
+            resultError=1e-6;
+        }//Of if
+
+        return resultError;
+    }//Of computeWeightedError
+}//Of class SimpleClassifier
---
+++ src/machineLearning/adaboosting/StumpClassifier.java
@@ -0,0 +1,12 @@
+package machineLearning.adaboosting;
+
+/**
+ * ClassName: StumpClassifier
+ * Package: machineLearning.adaboosting
+ * Description:
+ *
+ * @Author: luv_x_c
+ * @Create: 2023/8/17 20:37
+ */
+public class StumpClassifier {
+}
