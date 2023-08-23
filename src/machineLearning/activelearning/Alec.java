353e23cdfca2174d6cb2a45c3d15cf9415d14e2e
luvcodechen <chen_ney@qq.com>
2023-08-23 20:53:17 +0800

    java practice day66

--- src/machineLearning/activelearning/Alec.java
+++ src/machineLearning/activelearning/Alec.java
@@ -2,6 +2,9 @@ package machineLearning.activelearning;
 
 import weka.core.Instances;
 
+import java.io.FileReader;
+import java.io.IOException;
+
 /**
  * ClassName: Alec
  * Package: machineLearning.activelearning
@@ -34,7 +37,100 @@ public class Alec {
     /**
      * DistanceToMaster
      */
-    double[]distanceToMaster;
+    double[] distanceToMaster;
 
+    /**
+     * Sorted indices, where the first element indicates the instance with the biggest density,
+     */
+    int[] descendantDensities;
 
+    /**
+     * Priority.
+     */
+    double[] priority;
+
+    /**
+     * The maximal distance between any pair of points.
+     */
+    double maximalDistance;
+
+    /**
+     * Who is my master?
+     */
+    int[] masters;
+
+    /**
+     * Predicted labels.
+     */
+    int[] predictedLabels;
+
+    /**
+     * Instance status. 0 for unprocessed, 1 for queried, 2 for classified.
+     */
+    int[] instanceStatusArray;
+
+    /**
+     * The descendant indices to show the representatives of instances in a descendant order.
+     */
+    int[] descendantRepresentatives;
+
+    /**
+     * Indicate the cluster of each instance. It is only used in clusterInTwo(int[]).
+     */
+    int[] clusterIndices;
+
+    /**
+     * Blocks with size more than this threshold should not be split further.
+     */
+    int smallBlockThreshold = 3;
+
+    /**
+     * The constructor.
+     *
+     * @param paraFileName The data filename.
+     */
+    public Alec(String paraFileName) {
+        try {
+            FileReader fileReader = new FileReader(paraFileName);
+            dataset = new Instances(fileReader);
+            dataset.setClassIndex(dataset.numAttributes() - 1);
+            fileReader.close();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }//Of try
+
+        computeMaximalDistance();
+        clusterIndices = new int[dataset.numInstances()];
+    }//OF the constructor
+
+    /**
+     * Merge sort in descendant order to obtain an index array. The original array  is unchanged.
+     * The method should be tested further.
+     *
+     * @param paraArray The original array.
+     * @return The sorted indices.
+     */
+    public static int[] mergeSortToIndices(double[] paraArray) {
+        int tempLength = paraArray.length;
+        int[][] resultMatrix = new int[2][tempLength];
+
+        // Initialize
+        int tempIndex = 0;
+        for (int i = 0; i < tempLength; i++) {
+            resultMatrix[tempIndex][i] = i;
+        }//Of for i
+
+        // Merge
+        int tempCurrentLength=1;
+        // The indices for current merged groups.
+        int tempFirstStart,tempSecondStart,tempSecondEnd;
+
+        while (tempCurrentLength<tempLength){
+            // Divide into a number of groups.
+            // Here the boundary is adaptive to array length not equal to 2^k
+            for (int i = 0; i < Math.ceil((tempLength+0.0)/tempCurrentLength/2); i++) {
+                
+            }
+        }
+    }
 }
